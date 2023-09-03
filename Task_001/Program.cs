833d2c98b3846f62bd119ec5ecf1122cfbd2e437
Roman_Batrakov <kyct69@yandex.ru>
2023-09-03 22:02:47 +0300

    Решили задачу нахождения второго разряда трехзначного числа

---
+++ Task_001/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+11. Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+456--> 5
+782--> 8
+918--> 1 
+*/
+Console.WriteLine("Введите трёхзначное число: ");
+string strnumber = Console.ReadLine();
+int number = Convert.ToInt32(strnumber);
+if (number < 100 || number >= 1000)
+{
+    Console.WriteLine("Вы ввели неверное число");
+}
+else
+{
+    int TwoPairNumber = number / 10;
+    int FinalNumber = TwoPairNumber % 10;
+    Console.WriteLine("Второй разряд числа: " + FinalNumber);
+}
